db6893da6828e7a7aa62ec3d6024629235daac7a
Anish-Tripathi <tripathiarun780@gmail.com>
2025-05-19 14:13:08 +0530

    modified configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: "/", // Required for Vercel
+  base: "/",
   build: {
     outDir: "dist",
     emptyOutDir: true
